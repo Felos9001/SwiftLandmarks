0df57c0094ade50e82e7284fbb0627df6259e0ef
felos9001 <90056233+Felos9001@users.noreply.github.com>
2023-01-27 22:51:48 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Felipe Solorzano on 1/27/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
